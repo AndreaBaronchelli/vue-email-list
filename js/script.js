9cb618c3134c9e574d4a95a0dc4d24919957da6d
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-05-03 13:48:48 +0200

    script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Ok");
+const app = new Vue({
+    el: "#app",
+    data: {
+        emailList:[],
+    },
+    computed: {
+        dataOk(){
+            return this.emailList.length === 10
+        }
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(result => this.emailList.push(result.data.response)
+                .catch(error => console.log(error))
+            );
+        }
+    },
+    
+});
